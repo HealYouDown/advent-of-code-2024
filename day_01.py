a65ea9e4b442a6a01f8352eca4c7bc9f890013ed
Jeremy Regitz <jeremyregitz@gmail.com>
2024-12-01 10:38:01 +0100

    day 1

---
+++ day_01.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+left = []
+right = []
+with open("inputs/day_01.txt", "r") as fp:
+    for row in fp.read().splitlines():
+        if not row:
+            continue
+
+        a, b = row.split(" " * 3)
+        left.append(int(a))
+        right.append(int(b))
+
+left.sort()
+right.sort()
+
+diffs = []
+for a, b in zip(left, right):
+    diff = abs(a - b)
+    diffs.append(diff)
+
+print("Puzzle 1:", sum(diffs))
+
+right_counter = Counter(right)
+score = sum([i * right_counter.get(i, 0) for i in left])
+
+print("Puzzle 2:", score)
